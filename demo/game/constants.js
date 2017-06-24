4612f1d0ced2c32a08f673ef5000a28408770678
haven <haven@actio.com.au>
2017-06-24 12:25:42 +1000

    respawn distance tweak

--- demo/game/constants.js
+++ demo/game/constants.js
@@ -3,7 +3,7 @@ export const FLOOR = 235;
 export const ALIEN_FLOOR = 255;
 export const FACEHUGGER_FLOOR = 290;
 export const EGG_FLOOR = 315;
-export const RESPAWN_DISTANCE = 800;
+export const RESPAWN_DISTANCE = 1200;
 
 export const KILL_THRESHOLD = IS_MOBILE?40:40;
 
